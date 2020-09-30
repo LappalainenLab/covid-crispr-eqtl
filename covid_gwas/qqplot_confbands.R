9b7d13654b12924d229f411ec142a35f34b0b781
Silva Kasela <skasela@pe2cc3-044.c.nygenome.org>
2020-09-30 09:06:35 -0400

    Add scripts to estimate enrichment in COVID-19 GWAS

---
+++ covid_gwas/qqplot_confbands.R
@@ -0,0 +1,140 @@
+#------------------------------------------------
+# Function for QQ plot with confidence intervals
+#------------------------------------------------
+
+# Q-Q plot with confidence bands ----------------
+
+# library(ggplot2)
+# library(ggraster)
+
+qqplot_confbands <- function(pval,
+                             confidence = 0.95, confbands = TRUE,
+                             main = "Q-Q plot", color_group_name = "Trait 1",
+                             add_p = NULL, add_group = NULL,
+                             point_size = 2.5,
+                             raster_plot = FALSE,
+                             show_legend = FALSE) {
+  #-----------------
+  # add_p- NULL or list() of p-value vectors to add to the plot, add_p = list(x = sel_crispr_hits$all_inv_var_meta_p, y = random$all_inv_var_meta_p)
+  # add_group - NULL or list() of group names, add_group = list(x = "CRISPR", y = "Random")
+  # raster_plot - uses ggraster::geom_point_rast to create rasterized plots and keep text as vectors
+  #----------------
+
+  alpha <- 1 - confidence
+  n <- length(pval)
+  pval <- sort(pval)
+  k <- c(1:n)
+
+  lower <- qbeta(alpha/2, k, n + 1 - k)
+  upper <- qbeta((1 - alpha/2), k, n + 1 - k)
+  expected <- (1:n - 0.5)/n
+
+  max_p <- ceiling(max(-log10(pval),-log10(expected)))
+
+  # Prepare dataframe for plotting
+  df <- data.frame(expected = -log10(expected),
+                   observed = -log10(pval),
+                   lower = -log10(lower),
+                   upper = -log10(upper))
+
+  # Plot Q-Q plot
+  g <- ggplot(data = df, aes(x = expected, y = observed)) +
+      labs(title = main,
+           x = bquote(-log[10] ~ "(expected" ~ italic(P) ~ "-value)"),
+           y = bquote(-log[10] ~ "(observed" ~ italic(P) ~ "-value)")) +
+      geom_abline(slope = 1, intercept = 0) +
+      scale_x_continuous(limits = c(0, ceiling(max(-log10(df$expected))))) +
+      scale_y_continuous(limits = c(0, max_p))
+
+  if (raster_plot) {
+    g <- g +
+      geom_point_rast(aes(color = color_group_name), pch = 21, size = point_size)
+  } else {
+    g <- g +
+      geom_point(aes(color = color_group_name), pch = 21, size = point_size)
+  }
+
+  if (confbands) {
+    g <- g +
+      geom_line(data = df, aes(x = expected, y = lower), col = "grey65", lty = 2) +
+      geom_line(data = df, aes(x = expected, y = upper), col = "grey65", lty = 2)
+  }
+
+  if (!is.null(add_p)) {
+    show_legend <- TRUE # show legend, otherwise keep `show_legend = FALSE`
+    l <- length(add_p)
+    for (i in 1:l) {
+      n <- length(add_p[[i]])
+      expected <- (1:n - 0.5)/n
+
+      # Prepare dataframe for plotting
+      df_add <- data.frame(expected = -log10(expected),
+                           observed = -log10(sort(add_p[[i]])))
+
+      if (i == 1) {
+        add_group_name_1 <- ifelse(is.null(add_group[[i]]), paste0("Trait", i + 1), add_group[[i]])
+        if (raster_plot) {
+          g <- g +
+            geom_point_rast(data = df_add, aes(x = expected, y = observed, color = add_group_name_1), pch = 21, size = point_size)
+        } else {
+          g <- g +
+            geom_point(data = df_add, aes(x = expected, y = observed, color = add_group_name_1), pch = 21, size = point_size)
+        }
+      }
+      if (i == 2) {
+        add_group_name_2 <- ifelse(is.null(add_group[[i]]), paste0("Trait", i + 1), add_group[[i]])
+        if (raster_plot) {
+          g <- g +
+            geom_point_rast(data = df_add, aes(x = expected, y = observed, color = add_group_name_2), pch = 21, size = point_size)
+        } else {
+          g <- g +
+            geom_point(data = df_add, aes(x = expected, y = observed, color = add_group_name_2), pch = 21, size = point_size)
+        }
+      }
+      if (i == 3) {
+        add_group_name_3 <- ifelse(is.null(add_group[[i]]), paste0("Trait", i + 1), add_group[[i]])
+        if (raster_plot) {
+          g <- g +
+            geom_point_rast(data = df_add, aes(x = expected, y = observed, color = add_group_name_3), pch = 21, size = point_size)
+        } else {
+          g <- g +
+            geom_point(data = df_add, aes(x = expected, y = observed, color = add_group_name_3), pch = 21, size = point_size)
+        }
+      }
+      if (i == 4) {
+        add_group_name_4 <- ifelse(is.null(add_group[[i]]), paste0("Trait", i + 1), add_group[[i]])
+        if (raster_plot) {
+          g <- g +
+            geom_point_rast(data = df_add, aes(x = expected, y = observed, color = add_group_name_4), pch = 21, size = point_size)
+        } else {
+          g <- g +
+            geom_point(data = df_add, aes(x = expected, y = observed, color = add_group_name_4), pch = 21, size = point_size)
+        }
+      }
+      if (i == 5) {
+        add_group_name_5 <- ifelse(is.null(add_group[[i]]), paste0("Trait", i + 1), add_group[[i]])
+        if (raster_plot) {
+          g <- g +
+            geom_point_rast(data = df_add, aes(x = expected, y = observed, color = add_group_name_5), pch = 21, size = point_size)
+        } else {
+          g <- g +
+            geom_point(data = df_add, aes(x = expected, y = observed, color = add_group_name_5), pch = 21, size = point_size)
+        }
+      }
+      if (i > 5) {
+        warning("Up to 5 different groups allowed! Modify the function to add more!")
+      }
+    }
+  }
+  if (!show_legend) {
+    g <- g +
+      scale_color_manual(values = "royalblue") +
+      theme(legend.position = "none")
+  }
+  return(g)
+}
+
+# Calculate lambda for different chi-square distribution quantiles -------------
+
+lambda_quantile <- function(p, q = 0.5) quantile(qchisq(p, df = 1, lower.tail = FALSE), probs = q) / qchisq(q, df = 1)
+
